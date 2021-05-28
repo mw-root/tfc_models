5a1ca02fa9be20da6275fa717ee915412ae25c03
MWroot <mw@example.com>
2021-05-28 19:35:27 -0400

    checking err

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/alecthomas/jsonschema"
 )
@@ -14,14 +15,27 @@ func check(e error) {
 	}
 }
 
+var schemaFolder string = "./jsonschemas"
+
+func ensureSchemaFolder() {
+	if _, err := os.Stat(schemaFolder); os.IsNotExist(err) {
+		err := os.Mkdir(schemaFolder, 0755)
+		check(err)
+	}
+}
+
 func main() {
 
 	m := getStructs()
 
+	ensureSchemaFolder()
+
 	for file, iface := range m {
+
 		schema := jsonschema.Reflect(iface)
 		jschema, _ := json.MarshalIndent(schema, "", "    ")
-		filename := file + ".json"
+		filename := schemaFolder + "/" + file + ".json"
+
 		fmt.Printf("Writing %s\n", filename)
 		err := ioutil.WriteFile(filename, jschema, 0644)
 		check(err)
