06f00322b39b747803442b73e918ab117d8c9b5c
MWroot <mw@example.com>
2021-05-28 19:50:40 -0400

    EOD savegame

--- structs.go
+++ structs.go
@@ -9,6 +9,7 @@ func getStructs() map[string]interface{} {
 
 	s["workspace_create_options"] = &tfe.WorkspaceCreateOptions{}
 	s["workspace_list_options"] = &tfe.WorkspaceListOptions{}
+	s["workspace_lock_options"] = &tfe.WorkspaceLockOptions{}
 	s["workspace_update_options"] = &tfe.WorkspaceUpdateOptions{}
 
 	return s
